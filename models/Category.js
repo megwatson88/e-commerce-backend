76020c0de29032223eeef6421b9bafd47d4b2ffa
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-02 13:11:30 -0500

    initial commit

---
+++ models/Category.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id:{
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTERGER
+    },
+    category_name: {
+      type: DataType.STRING,
+      allowNull:false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
